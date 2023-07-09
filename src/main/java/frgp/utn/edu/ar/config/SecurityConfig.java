03e9d99a448d25cc5150cfc8ef6e25a288fa5fca
LeonardoDFX <128710684+LeonardoDFX@users.noreply.github.com>
2023-07-09 13:17:56 -0300

    BCrypt para Contraseñas
    Contraseñas seguras en SecurityConfig

--- src/main/java/frgp/utn/edu/ar/config/SecurityConfig.java
+++ src/main/java/frgp/utn/edu/ar/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package frgp.utn.edu.ar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,7 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		auth
-		.inMemoryAuthentication().withUser("localadmin").password("{noop}123456").roles("ADMIN");
+		.inMemoryAuthentication()
+		.withUser("localadmin")
+		.password("$2a$12$oDingzHIKvubzzOPT3Sr..CISToI1txQv/6B3reU2vp2HhnZj.4T6") //123456
+		.roles("ADMIN")
+		.and()
+		.withUser("tamara")
+		.password("$2a$12$8tILy447KTEuMNnI52cVYO2N3U/xL8/3eeYZ9cJ6aZH4nqnTvYeEK") //herrera
+		.roles("ADMIN");
 	}
 	
 	@Override
@@ -30,5 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().logout();
 	}
 	
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder(12);
+	}
 }
